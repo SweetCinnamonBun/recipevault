a9acf9ea4d5f3888187c81f5b4995cdd0201328b
Roman <roman.klemiato18@gmail.com>
2024-12-21 00:52:11 +0200

    database connection setup

---
+++ Infrastructure/Data/BlogContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public required DbSet<Category> Categories { get; set; }
+
+
+    }
+}
