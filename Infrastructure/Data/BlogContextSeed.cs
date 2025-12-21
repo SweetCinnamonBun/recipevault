a0cbeff1ebbe14ea02790d8fc4b6f5d33270e869
Roman <roman.klemiato18@gmail.com>
2025-12-21 22:11:50 +0200

    seeding roles

---
+++ Infrastructure/Data/BlogContextSeed.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Data
+{
+    public class BlogContextSeed
+    {
+        private static async Task SeedData(BlogContext context, UserManager<AppUser> userManager)
+        {
+
+        }
+    }
+}
