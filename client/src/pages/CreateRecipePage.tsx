128df043ebd94a54e758de61deffabd8d942dc45
Roman <roman.klemiato18@gmail.com>
2025-12-24 17:55:46 +0200

    added a modal to the create recipe page for the preview page

--- client/src/pages/CreateRecipePage.tsx
+++ client/src/pages/CreateRecipePage.tsx
@@ -10,6 +10,13 @@ import { useDropzone } from "react-dropzone";
 import { Category } from "@/types/Recipe";
 import { useCategories } from "@/lib/hooks/useCategories";
 import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { addCategories, addIngredients, addInstructions, setRecipe } from "@/store/recipeSlice";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import Modal from "@/components/Modal";
+import { MdAccessTime } from "react-icons/md";
+import { PiForkKnifeFill, PiShootingStarLight } from "react-icons/pi";
 
 type AddIngredient = {
   quantity: string;
@@ -36,6 +43,16 @@ const CreateRecipePage = () => {
   const [newInstruction, setNewInstruction] = useState<AddInstruction>({
     text: "",
   });
+
+  const recipePreview = useSelector((state: RootState) => state.recipe);
+    const ingredientsPreview = useSelector(
+    (state: RootState) => state.recipe.ingredients
+  );
+  const instructionsPreview = useSelector(
+    (state: RootState) => state.recipe.instructions
+  );
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
   
   const { postImage } = useImages();
   const { createRecipe } = useRecipes();
@@ -62,7 +79,7 @@ const CreateRecipePage = () => {
   });
 
   const navigate = useNavigate();
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const handleUnitChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setTimeUnit(e.target.value);
@@ -119,14 +136,14 @@ const CreateRecipePage = () => {
       const createdRecipe = await createRecipe.mutateAsync(recipeData);
       const recipeId = createdRecipe.id;
 
-      // dispatch(setRecipe(createdRecipe));
+      dispatch(setRecipe(createdRecipe));
 
       if (selectedCategories.length > 0) {
         await addCategoriesToRecipe.mutateAsync({
           recipeId,
           categoryIds: selectedCategories.map((x) => x.id),
         });
-        // dispatch(addCategories(selectedCategories))
+        dispatch(addCategories(selectedCategories))
       }
 
       if (ingredients.length > 0) {
@@ -142,6 +159,8 @@ const CreateRecipePage = () => {
         if (!ingredientsResponse.ok) {
           throw new Error("Failed to add ingredients");
         }
+
+        dispatch(addIngredients(ingredients))
       }
 
       if (instructions.length > 0) {
@@ -159,6 +178,7 @@ const CreateRecipePage = () => {
         }
 
         // Save to Redux
+        dispatch(addInstructions(instructions))
       }
 
       navigate("/dashboard");
@@ -427,6 +447,89 @@ const CreateRecipePage = () => {
           </button>
         </form>
       </div>
+      {isModalOpen && (
+      <Modal onClose={() => setIsModalOpen(false)}>
+        <div className="flex flex-col items-center">
+              <h1 className="w-full py-2 mt-5 mb-20 text-4xl italic text-center bg-white rounded-lg">
+                Recipe Preview
+              </h1>
+              <h1 className="my-5 text-3xl">{recipePreview?.name}</h1>
+              <div className="flex justify-between px-5 py-5 mt-2 mb-8 w-96 gap-x-5">
+                <div className="">
+                  <div className="flex flex-col items-center">
+                    <MdAccessTime className="w-8 h-8" />
+                    <span className="text-lg">{recipePreview?.cookingTime}</span>
+                    <span className="text-md">Cooking time</span>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="flex flex-col items-center">
+                    <PiShootingStarLight className="w-8 h-8" />
+                    <span className="text-lg">{recipePreview?.difficulty}</span>
+                    <span className="text-md">Difficulty</span>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="flex flex-col items-center">
+                    <PiForkKnifeFill className="w-8 h-8" />
+                    <span className="text-lg">{recipePreview?.servingSize}</span>
+                    <span className="text-md">Serving Size</span>
+                  </div>
+                </div>
+              </div>
+              <figure className="w-full sm:w-3/4 px-1 lg:w-[70%] [@media(min-width:1100px)]:w-[60%] [@media(min-width:1300px)]:w-[50%] 2xl:w-[50%] [@media(min-width:1750px)]:w-[40%]">
+                <img
+                  src={recipePreview?.imageUrl}
+                  alt={recipePreview?.name}
+                  className="w-full h-[380px] md:h-[520px] rounded-xl"
+                />
+              </figure>
+              <div className="flex flex-wrap justify-center mt-12 gap-y-5">
+                {recipePreview?.categories.map((category: Category, index: number) => (
+                  <span key={index} className="px-4 py-2 ml-4 bg-[#00FF9C] rounded-lg">
+                    {category.name}
+                  </span>
+                ))}
+              </div>
+              <section className="w-11/12 my-10 2xl:px-52">
+                <div className="w-full p-8 text-xl bg-white shadow-lg rounded-xl h-96">
+                  {recipePreview?.description}
+                </div>
+              </section>
+              <section className="grid w-11/12 gap-y-4 grid-cols-1 md:grid-cols-2  md:h-[750px] gap-x-8 2xl:px-52">
+                <div className="p-6 shadow-lg rounded-lg bg-[#F8FAE5]">
+                  <h2 className="my-2 text-2xl font-bold">Ingredients</h2>
+                  <ul className="p-2 space-y-4 list-disc">
+                    {recipePreview?.ingredients.map((ingredient) => (
+                      <li className="space-x-2 text-xl">
+                        <span>{ingredient.quantity}</span>
+                        <span>{ingredient.unit}</span>
+                        <span>{ingredient.name}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div className="p-6 shadow-lg rounded-lg bg-[#F8FAE5]">
+                  <h2 className="my-2 text-2xl font-bold">Instructions</h2>
+                  <ul className="p-2 space-y-4 list-disc">
+                    {recipePreview?.instructions.map((instruction) => (
+                      <li className="text-xl">{instruction.text}</li>
+                    ))}
+                  </ul>
+                </div>
+              </section>
+              {/* <button
+                className={`px-6 py-3 text-xl  rounded-lg my-14 ${
+                  isLoading ? "bg-green-100" : "bg-green-400"
+                }`}
+                onClick={handleSubmit}
+                disabled={isLoading}
+              >
+                Create Recipe
+              </button> */}
+            </div>
+      </Modal>
+      )}
     </>
   );
 };
