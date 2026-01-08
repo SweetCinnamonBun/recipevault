cc180bf0777f14c28aea62a40cd531b929e7d148
Roman <roman.klemiato18@gmail.com>
2026-01-08 22:10:37 +0200

    fixed card-grid sizing issue

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -5,7 +5,7 @@ import { PiForkKnifeFill } from "react-icons/pi";
 import { Link } from "react-router";
 import SearchBox from "@/components/SearchBox";
 import { CiFilter } from "react-icons/ci";
-import { FaSort } from "react-icons/fa";
+import { FaSort, FaStar } from "react-icons/fa";
 import Modal from "@/components/Modal";
 import RecipeStars from "@/components/RecipeStars";
 import { useInfiniteQuery } from "@tanstack/react-query";
@@ -67,28 +67,23 @@ const HomePage = () => {
     setIsFiltersModalOpen(false);
   };
 
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isLoading
-  } = useInfiniteQuery({
-    queryKey: ["recipes", appliedCategories, sortBy, isAscending],
-    queryFn: ({ pageParam = 1 }) =>
-      fetchRecipes(pageParam, 5, appliedCategories, sortBy, isAscending),
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages) => {
-      const currentPage = allPages.length;
-      const totalPages = lastPage?.totalPages;
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+    useInfiniteQuery({
+      queryKey: ["recipes", appliedCategories, sortBy, isAscending],
+      queryFn: ({ pageParam = 1 }) =>
+        fetchRecipes(pageParam, 5, appliedCategories, sortBy, isAscending),
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPages) => {
+        const currentPage = allPages.length;
+        const totalPages = lastPage?.totalPages;
 
-      console.log("Current Page:", currentPage, "Total Pages:", totalPages);
+        console.log("Current Page:", currentPage, "Total Pages:", totalPages);
 
-      return currentPage < totalPages ? currentPage + 1 : undefined;
-    },
-    refetchOnWindowFocus: false,
-    staleTime: 20_000,
-  });
+        return currentPage < totalPages ? currentPage + 1 : undefined;
+      },
+      refetchOnWindowFocus: false,
+      staleTime: 20_000,
+    });
 
   useEffect(() => {
     if (inView && hasNextPage && !isFetchingNextPage) {
@@ -96,7 +91,7 @@ const HomePage = () => {
       setTimeout(() => {
         fetchNextPage();
         setShowSpinner(false);
-      }, 3000); // Adjust delay time as needed
+      }, 100); // Adjust delay time as needed
     }
   }, [inView, fetchNextPage, hasNextPage, isFetchingNextPage]);
 
@@ -181,59 +176,60 @@ const HomePage = () => {
       <div className=" xl:px-10  2xl:px-36 [@media(min-width:1000px)]: [@media(min-width:1750px)]:px-48 [@media(min-width:1900px)]:px-56">
         {isLoading ? (
           <div className="flex items-center justify-center w-full h-[70vh]">
-          <ClipLoader color="#0a0301" size={50} />
-        </div>
+            <ClipLoader color="#0a0301" size={50} />
+          </div>
         ) : (
-          <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-y-10 justify-items-center">
-              <>
-                {recipes?.map((recipe: Recipe, index: number) => (
-                  <Link
-                    to={`/recipe/${recipe.id}`}
-                    key={index}
-                    onClick={() =>
-                      window.scrollTo({ top: 0, behavior: "smooth" })
-                    }
-                  >
-                    <div
-                      key={recipe.id}
-                      className=" w-[356px] h-[400px] bg-white rounded-xl cursor-pointer hover:border hover:border-orange-300 hover:shadow-lg [@media(min-width:1450px)]:w-[380px]"
-                    >
-                      <figure className="w-full h-60 ">
-                        <img
-                          src={recipe.imageUrl}
-                          alt={recipe.name}
-                          className="w-full h-full rounded-t-xl"
-                        />
-                      </figure>
-                      <div className="px-3">
-                        <h2 className="py-2 text-xl text-red-700">
-                          {recipe.name}
-                        </h2>
-                        <div className="flex items-center gap-2">
-                          {recipe.categories?.slice(0, 2).map((category) => (
-                            <span className="px-3 py-1 bg-[#00FF9C] rounded-lg">{category.name}</span>
-                          ))}
-                        </div>
-                        <div className="flex gap-5 mt-2">
-                          <div className="flex gap-1">
-                            <MdAccessTime className="w-7 h-7" />
-                            <p>{recipe.cookingTime}</p>
+          <div className="grid px-4 gap-x-6 md:grid-cols-2 xl:grid-cols-3 gap-y-10 ">
+            <>
+              {recipes?.map((recipe: Recipe, index: number) => (
+                <Link
+                  className=""
+                  to={`/recipe/${recipe.id}`}
+                  key={index}
+                  onClick={() =>
+                    window.scrollTo({ top: 0, behavior: "smooth" })
+                  }
+                >
+                  <div className="w-full bg-white rounded-2xl hover:shadow-md hover:bg-gray-50">
+                    <figure className="h-72 rounded-2xl">
+                      <img
+                        src={recipe?.imageUrl}
+                        alt="recipe image"
+                        className="object-cover w-full h-full rounded-t-2xl"
+                      />
+                    </figure>
+                    <div className="px-3 pb-4 mt-2 space-y-2 card-body">
+                      <h3 className="text-xl ">{recipe?.name}</h3>
+                      <div className="flex items-center gap-2">
+                        {recipe.categories?.slice(0, 2).map((category: Category) => (
+                          <span key={category.slug} className="px-3 py-1 text-sm bg-green-300 rounded-xl">{category.name}</span>
+                        ))}
+                      </div>
+                      <div className="flex gap-3 basic-info ">
+                        <div className="flex items-center gap-1">
+                          <div className="px-1 py-1 bg-blue-200 rounded-full">
+                            <MdAccessTime className="w-4 h-4" />
                           </div>
-                          <div className="flex gap-1">
-                            <PiForkKnifeFill className="w-7 h-7" />
-                            <p>{recipe.servingSize || 5}</p>
-                          </div>
+                          <span>{recipe?.cookingTime}</span>
                         </div>
-                        <div className="flex pt-2 ">
-                          <RecipeStars
-                            averageRating={recipe.averageRating || 0}
-                          />
+                        <div className="flex items-center gap-1">
+                          <div className="px-1 py-1 bg-blue-200 rounded-full">
+                            <PiForkKnifeFill className="w-4 h-4" />
+                          </div>
+                          <span>{recipe?.servingSize}</span>
+                        </div>
+                        <div className="flex items-center gap-1">
+                          <div className="px-1 py-1 bg-blue-200 rounded-full">
+                            <FaStar className="w-4 h-4" fill="yellow" />
+                          </div>
+                          <span>{recipe?.averageRating}</span>
                         </div>
                       </div>
                     </div>
-                  </Link>
-                ))}
-              </>
+                  </div>
+                </Link>
+              ))}
+            </>
           </div>
         )}
         <div ref={ref} className="flex justify-center my-4">
